d5fae823f231bfa5f1dbb68434bf2176ccff2b82
Mahamudul hossainn <md.mahamudulhossainn@gmail.com>
2022-03-05 19:43:39 +0600

    Admin Login

--- school_management/routes/web.php
+++ school_management/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin',[AdminController::class,'admin_login_form']);
+Route::post('/admin_login',[AdminController::class,'admin_login']);
+Route::get('/dashboard',[DashboardController::class,'home']);
+
