a6d264467108c062713ba3052555ae9e5201f891
Mahamudul hossainn <md.mahamudulhossainn@gmail.com>
2022-03-12 15:04:49 +0600

    Assign Subject Part-07

--- school_management/app/Http/Controllers/AssignSubjectController.php
+++ school_management/app/Http/Controllers/AssignSubjectController.php
@@ -67,4 +67,37 @@ class AssignSubjectController extends Controller
         $data['group'] = Group::all();
         return view('admin.setup.assign_subject.assign_subject_edit',$data);
     }
+    public function update(Request $req,$class_id,$group_id){
+        //dd($req->all());
+        AssignSubject::where(['class_name_id'=>$class_id,'group_id'=>$group_id])->delete();
+        if($req->class_name_id > 0){
+            for($i=0; $i< count($req->subject_id); $i++){
+                $data = new AssignSubject;
+                $data->class_name_id = $req->class_name_id;
+                if($req->group_id == null){
+                    $data->group_id = 0;
+                }else{
+                    $data->group_id = $req->group_id;
+                }
+                $data->subject_id = $req->subject_id[$i];
+                $data->subjective = $req->subjective[$i];
+                $data->subjective_pass_mark = $req->subjective_pass_mark[$i];
+                if($req->objective[$i] == null || $req->objective_pass_mark[$i]){
+                    $data->objective = 0;
+                    $data->objective_pass_mark = 0;
+                }else{
+                    $data->objective = $req->objective[$i];
+                    $data->objective_pass_mark = $req->objective_pass_mark[$i];
+                }
+                if($req->subjective[$i] != null){
+                    $data->full_mark = $req->subjective[$i] + $req->objective[$i];
+                }else{
+                    $data->full_mark = $req->subjective[$i];
+                }
+                $data->save();
+            }
+        }
+        $req->session()->flash('message','Assigned Subject Updated Successfully');
+        return redirect('/assign_subject_list');
+    }
 }
